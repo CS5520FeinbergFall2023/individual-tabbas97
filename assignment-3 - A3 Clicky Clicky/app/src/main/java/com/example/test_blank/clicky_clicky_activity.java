427ed20d597d9d366e24e10d154b8610e5c33bc2
Thameem Abbas <tabbas97@gmail.com>
2023-09-22 11:06:22 -0400

    FIX: Remove comments and Text when not pressed

--- assignment-3 - A3 Clicky Clicky/app/src/main/java/com/example/test_blank/clicky_clicky_activity.java
+++ assignment-3 - A3 Clicky Clicky/app/src/main/java/com/example/test_blank/clicky_clicky_activity.java
@@ -21,13 +21,7 @@ public class clicky_clicky_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky_clicky);
         
-//        // Set up a map of buttons to their respective ids
-//        buttonMap.put("buttonA", "A");
-//        buttonMap.put("buttonB", "B");
-//        buttonMap.put("buttonC", "C");
-//        buttonMap.put("buttonD", "D");
-//        buttonMap.put("buttonE", "E");
-//        buttonMap.put("buttonF", "F");
+        updateText("Pressed: -");
         
         // Set up the button listeners
         for (String buttonId : buttonMap.keySet()) {
@@ -35,23 +29,28 @@ public class clicky_clicky_activity extends AppCompatActivity {
             button.setOnClickListener(new android.view.View.OnClickListener() {
                 @Override
                 public void onClick(android.view.View v) {
-                    updateText(buttonId);
+                    updateButtonName(buttonId);
                 }
             });
         }
         
     }
     
-    protected void updateText(String buttonId) {
+    protected void updateButtonName(String buttonId) {
         
         // Get the button text
-        String buttonText = "Pressed : " + buttonMap.get(buttonId);
+        String buttonText = "Pressed: " + buttonMap.get(buttonId);
+        
+        updateText(buttonText);
+    }
+    
+    protected void updateText(String text) {
         
         // Get the text view
         android.widget.TextView textView = findViewById(R.id.pressed_button_text_view);
         
         // Update the text view
-        textView.setText(buttonText);
+        textView.setText(text);
     }
     
 }
