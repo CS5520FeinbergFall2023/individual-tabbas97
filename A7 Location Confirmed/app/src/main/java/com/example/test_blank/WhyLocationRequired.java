8173b2b8353d08a275de80108215280982f38714
Thameem Abbas <tabbas97@gmail.com>
2023-11-05 17:55:00 -0500

    ADD: Assignment 7

---
+++ A7 Location Confirmed/app/src/main/java/com/example/test_blank/WhyLocationRequired.java
@@ -0,0 +1,14 @@
+package com.example.test_blank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhyLocationRequired extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_why_location_required);
+    }
+}
---
+++ assignment-7 - A7 Location Confirmed/app/src/main/java/com/example/test_blank/GPSDistanceTracker.java
@@ -0,0 +1,2 @@
+package com.example.test_blank;public class GPSDistanceTracker {
+}
